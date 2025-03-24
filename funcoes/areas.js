fe746794d45c46e7c023d6c232ca582895aacc60
hemillesferreira <hemillesferreira@hotmail.com>
2025-03-24 16:37:03 -0300

    up

---
+++ funcoes/areas.js
@@ -0,0 +1,8 @@
+function calcularAreaRetangulo (l,a){
+    if(typeof l !== "number" || typeof a !== "number"){
+        alert("apenas numeros")
+        return null
+    }
+       
+    return l*a
+}
